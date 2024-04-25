bfd7363d162a04a429afc3efc3cbd732ada3cb5f
Liam Gruber <main@dnascanner.de>
2024-04-25 19:26:39 +0200

    Reformatted index.html + switched to typescript <3

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let keyInputs = "";
-
 document.addEventListener("keydown", (event) => {
-	keyInputs += event.key;
-
-	if (!keyInputs.toLowerCase().endsWith("rawr")) return;
-	document.body.classList.add("rotate");
+    keyInputs += event.key;
+    if (!keyInputs.toLowerCase().endsWith("rawr"))
+        return;
+    document.body.classList.add("rotate");
 });
+const funnyTextContainer = document.querySelector(".container2 > .box > div");
+for (let i = 1; i <= 32; i++) {
+    const span = document.createElement("span");
+    span.style.setProperty("--i", i.toString());
+    funnyTextContainer?.appendChild(span);
+}
