3f26a87202a467259257da4bca44250b82c49968
DNAScanner <main@dnascanner.de>
2024-05-23 17:27:00 +0200

    Switched entirely to sass instead of pure css Added source maps Removed cancer from code Removed node installation package

--- index.js
+++ index.js
@@ -6,10 +6,83 @@ document.addEventListener("keydown", (event) => {
         return;
     document.body.classList.add("rotate");
 });
-const funnyTextContainer = document.querySelector(".transition > div > div > div");
+const funnyTextWrapper = document.querySelector(".transition > div > div > div");
 for (let i = 1; i <= 32; i++) {
     const span = document.createElement("span");
     span.style.setProperty("--i", i.toString());
-    funnyTextContainer === null || funnyTextContainer === void 0 ? void 0 : funnyTextContainer.appendChild(span);
+    funnyTextWrapper === null || funnyTextWrapper === void 0 ? void 0 : funnyTextWrapper.appendChild(span);
+}
+const socials = [
+    {
+        name: "Discord",
+        link: "https://discord.com/users/1088952298962681946",
+        icon: "cards/discord.svg",
+    },
+    {
+        name: "YouTube",
+        link: "https://www.youtube.com/channel/UCyxexTnvTeDsQekNVPHtVBg",
+        icon: "cards/youtube.svg",
+    },
+    {
+        name: "Steam",
+        link: "https://steamcommunity.com/id/GDFlorian/",
+        icon: "cards/steam.svg",
+    },
+    {
+        name: "GitHub",
+        link: "https://github.com/Floerianc/",
+        icon: "cards/github.svg",
+    },
+];
+const socialsWrapper = document.querySelector(".socials");
+for (const social of socials) {
+    const a = document.createElement("a");
+    a.href = social.link;
+    a.target = "_blank";
+    a.classList.add("socials-card");
+    const img = document.createElement("img");
+    img.src = social.icon;
+    img.classList.add("socials-picture");
+    a.appendChild(img);
+    socialsWrapper === null || socialsWrapper === void 0 ? void 0 : socialsWrapper.appendChild(a);
+}
+const projects = [
+    {
+        name: "PythonIDE",
+        description: "This is a VERY mediocre little IDE I created in Python, for Python.",
+        link: "https://github.com/Floerianc/FloriansIDE",
+    },
+    {
+        name: "pdfX",
+        description: "With pdfX you can extract the text from a PDF and save it in a .txt file.",
+        link: "https://github.com/Floerianc/pdfX",
+    },
+    {
+        name: "Encrypter",
+        description: "With the Encrypter you can encrypt text messages, save their decryption codes in a small text file and then decrypt them via that text file easily.",
+        link: "https://github.com/Floerianc/Encrypter",
+    },
+    {
+        name: "OpenWeather",
+        description: "OpenWeather is a little program I made as a joke to determine the weather details in my city and a couple other cities around the world aswell.",
+        link: "https://github.com/Floerianc/OpenWeatherAPI-Program",
+    },
+];
+const projectsWrapper = document.querySelector(".main-content > .text-start");
+for (const project of projects) {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("project");
+    const h3 = document.createElement("h3");
+    h3.textContent = project.name;
+    const p = document.createElement("p");
+    p.textContent = project.description;
+    const a = document.createElement("a");
+    a.href = project.link;
+    a.target = "_blank";
+    a.textContent = "View on GitHub";
+    wrapper.appendChild(h3);
+    wrapper.appendChild(p);
+    wrapper.appendChild(a);
+    projectsWrapper === null || projectsWrapper === void 0 ? void 0 : projectsWrapper.appendChild(wrapper);
 }
 //# sourceMappingURL=index.js.map
